c8f5ec3576cb5a247ac786fe4a07e6a30c9d5930
Mikey71212 <michaelholton712@gmail.com>
2022-09-21 13:54:39 +1000

    Added Connect 4 game mode

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+// gameData stores all of the information to render the different game modes of the app. Information
 const gameData = {
-    '3 x 3': {
+    "3 x 3": {
         squarePerLine: 3,
-        desktopContainerWidth: '50vh',
-        desktopContainerHeight: '50vh',
-        mobileContainerWidth: '45vh',
-        mobileContainerHeight: '45vh',
+        desktopContainerWidth: "50vh",
+        desktopContainerHeight: "50vh",
+        mobileContainerWidth: "45vh",
+        mobileContainerHeight: "45vh",
 
         winConditions: {
             row1: [1, 2, 3],
@@ -15,15 +16,15 @@ const gameData = {
             col3: [3, 6, 9],
             diag1: [1, 5, 9],
             diag2: [3, 5, 7],
-        }
+        },
     },
 
-    '4 x 4': {
+    "4 x 4": {
         squarePerLine: 4,
-        desktopContainerWidth: '60vh',
-        desktopContainerHeight: '60vh',
-        mobileContainerWidth: '45vh',
-        mobileContainerHeight: '45vh',
+        desktopContainerWidth: "60vh",
+        desktopContainerHeight: "60vh",
+        mobileContainerWidth: "45vh",
+        mobileContainerHeight: "45vh",
 
         winConditions: {
             row1: [1, 2, 3, 4],
@@ -36,15 +37,15 @@ const gameData = {
             col4: [4, 8, 12, 16],
             diag1: [1, 6, 11, 16],
             diag2: [4, 7, 10, 13],
-        }
+        },
     },
 
-    '5 x 5': {
+    "5 x 5": {
         squarePerLine: 5,
-        desktopContainerWidth: '65vh',
-        desktopContainerHeight: '65vh',
-        mobileContainerWidth: '45vh',
-        mobileContainerHeight: '45vh',
+        desktopContainerWidth: "65vh",
+        desktopContainerHeight: "65vh",
+        mobileContainerWidth: "45vh",
+        mobileContainerHeight: "45vh",
 
         winConditions: {
             row1: [1, 2, 3, 4, 5],
@@ -59,26 +60,73 @@ const gameData = {
             col5: [5, 10, 15, 20, 25],
             diag1: [1, 7, 13, 19, 25],
             diag2: [5, 9, 13, 17, 21],
-        }
-    }
+        },
+    },
+
+    "Connect 4": {
+        squarePerLine: 5,
+        desktopContainerWidth: "65vh",
+        desktopContainerHeight: "65vh",
+        mobileContainerWidth: "45vh",
+        mobileContainerHeight: "45vh",
+
+        winConditions: {
+            row1: [1, 2, 3, 4],
+            row2: [6, 7, 8, 9],
+            row3: [11, 12, 13, 14],
+            row4: [16, 17, 18, 19],
+            row5: [21, 22, 23, 24],
+            row6: [2, 3, 4, 5],
+            row7: [7, 8, 9, 10],
+            row8: [12, 13, 14, 15],
+            row9: [17, 18, 19, 20],
+            row10: [22, 23, 24, 25],
+            col1: [1, 6, 11, 16],
+            col2: [2, 7, 12, 17],
+            col3: [3, 8, 13, 18],
+            col4: [4, 9, 14, 19],
+            col5: [5, 10, 15, 20],
+            col6: [6, 11, 16, 21],
+            col7: [7, 12, 17, 22],
+            col8: [8, 13, 18, 23],
+            col9: [9, 14, 19, 24],
+            col10: [10, 15, 20, 25],
+            diag1: [1, 7, 13, 19],
+            diag2: [5, 9, 13, 17],
+            diag3: [7, 13, 19, 25],
+            diag4: [9, 13, 17, 21],
+            diag5: [6, 12, 18, 24],
+            diag6: [2, 8, 14, 20],
+            diag7: [16, 12, 8, 4],
+            diag8: [22, 18, 14, 10],
+        },
+
+        insertLines: {
+            line1: [1, 6, 11, 16, 21],
+            line2: [2, 7, 12, 17, 22],
+            line3: [3, 8, 13, 18, 23],
+            line4: [4, 9, 14, 19, 24],
+            line5: [5, 10, 15, 20, 25],
+        },
+    },
 }
 
-const header = document.querySelector('header')
-const gameContainer = document.querySelector('.game-container')
-const outerContainer = document.querySelector('.outer-container')
-const gridSizeSelectors = document.querySelectorAll('.grid-size-selector')
-const gridSizeDiv = document.querySelector('#grid-size')
-const dropDown = document.querySelector('#dropdown')
-const plug = document.querySelector('#plug')
-const winTextDiv = document.querySelector('.win-text')
-const scoreDiv = document.querySelector('.win-container')
-const xScore = document.querySelector('#p1score')
-const oScore = document.querySelector('#p2score')
+const header = document.querySelector("header")
+const gameContainer = document.querySelector(".game-container")
+const outerContainer = document.querySelector(".outer-container")
+const gridSizeSelectors = document.querySelectorAll(".grid-size-selector")
+const gridSizeDiv = document.querySelector("#grid-size")
+const dropDown = document.querySelector("#dropdown")
+const plug = document.querySelector("#plug")
+const winTextDiv = document.querySelector(".win-text")
+const scoreDiv = document.querySelector(".win-container")
+const xScore = document.querySelector("#p1score")
+const oScore = document.querySelector("#p2score")
 
-const restartButton = document.createElement('button')
-restartButton.type = 'button'
-restartButton.classList.add('restart')
-restartButton.classList.add('win')
+const restartButton = document.createElement("button")
+restartButton.type = "button"
+restartButton.classList.add("restart")
+restartButton.classList.add("win")
 restartButton.innerHTML = '<img id="reset" src="./images/reset.png">Restart'
 
 const cross = `<img id="cross" src="./images/cross.png">`
@@ -88,28 +136,27 @@ const circlePreview = `<img src="./images/circle.png" class="preview">`
 let crossSlots = []
 let circleSlots = []
 
-let currentGridSize = '3 x 3'
+let currentGridSize = "3 x 3"
 let lineSize = 3
 let turn = 0
 let crossPoints = 0
 let circlePoints = 0
 
 const winAnimation1 = (target) => {
-    target.style.width = '100%'
-    target.style.height = '100%'
-    target.style.filter = 'blur(2px)'
-    // target.style.filter = 'opacity(50%)'
+    target.style.width = "100%"
+    target.style.height = "100%"
+    target.style.filter = "blur(2px)"
 }
 const winAnimation2 = (target) => {
-    target.style.width = '84%'
-    target.style.height = '84%'
-    target.style.filter = ''
+    target.style.width = "84%"
+    target.style.height = "84%"
+    target.style.filter = ""
 }
 
 const winScreen = (winner) => {
-    const winnerText = document.createElement('p')
-    winnerText.classList.add('win')
-    if (winner === 'draw') {
+    const winnerText = document.createElement("p")
+    winnerText.classList.add("win")
+    if (winner === "draw") {
         winnerText.textContent = `It's a draw.`
     } else {
         winnerText.textContent = `${winner} wins!`
@@ -118,19 +165,17 @@ const winScreen = (winner) => {
     oScore.textContent = circlePoints
     winTextDiv.appendChild(winnerText)
     winTextDiv.appendChild(restartButton)
-    winTextDiv.style.opacity = 1;
+    winTextDiv.style.opacity = 1
 }
 
 const checkForWin = () => {
-    if (turn === lineSize ** 2) {
-        winScreen('draw')
-        return
-    }
     const winConditionsObject = gameData[currentGridSize].winConditions
+
     for (let condition in winConditionsObject) {
         const requiredSlots = winConditionsObject[condition].length
         let circleCount = 0
         let crossCount = 0
+
         for (let slot of winConditionsObject[condition]) {
             if (circleSlots.includes(String(slot))) {
                 circleCount++
@@ -138,13 +183,18 @@ const checkForWin = () => {
                 crossCount++
             }
         }
+
         if (crossCount === requiredSlots || circleCount === requiredSlots) {
             for (let i = 1; i <= lineSize ** 2; i++) {
                 const currSlot = document.querySelector(`[data-square-num = "${i}"]`)
-                currSlot.removeEventListener('click', insertSymbol)
-                currSlot.removeEventListener('mouseover', previewSymbol)
-                currSlot.removeEventListener('mouseleave', unPreviewSymbol)
+                currSlot.removeEventListener("click", insertSymbol)
+                currSlot.removeEventListener("mouseover", previewSymbol)
+                currSlot.removeEventListener("mouseleave", unPreviewSymbol)
+                currSlot.removeEventListener("click", insertSymbolConnect4)
+                currSlot.removeEventListener("mouseover", previewSymbolC4)
+                currSlot.removeEventListener("mouseleave", unPreviewSymbolC4)
             }
+
             if (crossCount === requiredSlots) {
                 crossPoints += 1
                 for (let slot of winConditionsObject[condition]) {
@@ -152,7 +202,8 @@ const checkForWin = () => {
                     setTimeout(() => winAnimation1(currSlot), 400)
                     setTimeout(() => winAnimation2(currSlot), 800)
                 }
-                setTimeout(() => winScreen('Cross'), 1400)
+                setTimeout(() => winScreen("Cross"), 1400)
+                return
             } else if (circleCount === requiredSlots) {
                 circlePoints += 1
                 for (let slot of winConditionsObject[condition]) {
@@ -160,9 +211,13 @@ const checkForWin = () => {
                     setTimeout(() => winAnimation1(currSlot), 400)
                     setTimeout(() => winAnimation2(currSlot), 800)
                 }
-                setTimeout(() => winScreen('Circle'), 1400)
+                setTimeout(() => winScreen("Circle"), 1400)
+                return
             }
         }
+    }
+    if (turn === lineSize ** 2) {
+        winScreen("draw")
     }
 }
 
@@ -174,7 +229,49 @@ const insertSymbol = (event) => {
         event.target.innerHTML = circle
         circleSlots.push(event.target.dataset.squareNum)
     }
-    event.target.removeEventListener('click', insertSymbol)
+    event.target.removeEventListener("click", insertSymbol)
+    turn++
+    checkForWin()
+}
+
+const insertSymbolConnect4 = (event) => {
+    let currLine = null
+    let addShapeSlot = ""
+    targetNumId = event.target.dataset.squareNum
+
+    for (let line in gameData[currentGridSize].insertLines) {
+        const checkLine = gameData[currentGridSize].insertLines[line]
+        if (checkLine.includes(Number(targetNumId))) {
+            currLine = checkLine
+            break
+        }
+    }
+
+    const endOfLine = document.querySelector(`[data-square-num = "${currLine[4]}"]`)
+    if (endOfLine.innerHTML === "" || endOfLine.innerHTML === circlePreview || endOfLine.innerHTML === crossPreview) {
+        addShapeSlot = document.querySelector(`[data-square-num = "${currLine[4]}"]`)
+    } else {
+        for (let num of currLine) {
+            const selectedSlot = document.querySelector(`[data-square-num = "${num}"]`)
+            if (selectedSlot.innerHTML === cross || selectedSlot.innerHTML === circle) {
+                addShapeSlot = document.querySelector(`[data-square-num = "${num - 5}"]`)
+                break
+            }
+        }
+    }
+
+    addShapeSlot.removeEventListener("click", insertSymbolConnect4)
+    addShapeSlot.removeEventListener("mouseover", previewSymbolC4)
+    addShapeSlot.removeEventListener("mouseleave", unPreviewSymbolC4)
+
+    if (turn % 2 === 0) {
+        addShapeSlot.innerHTML = cross
+        crossSlots.push(addShapeSlot.dataset.squareNum)
+    } else {
+        addShapeSlot.innerHTML = circle
+        circleSlots.push(addShapeSlot.dataset.squareNum)
+    }
+
     turn++
     checkForWin()
 }
@@ -190,6 +287,69 @@ const previewSymbol = (event) => {
 const unPreviewSymbol = (event) => {
     if (event.target.innerHTML === crossPreview || event.target.innerHTML === circlePreview) {
         event.target.innerHTML = ""
+    }
+}
+
+const previewSymbolC4 = (event) => {
+    let currLine = null
+    let addShapeSlot = ""
+    targetNumId = event.target.dataset.squareNum
+
+    for (let line in gameData[currentGridSize].insertLines) {
+        const checkLine = gameData[currentGridSize].insertLines[line]
+        if (checkLine.includes(Number(targetNumId))) {
+            currLine = checkLine
+            break
+        }
+    }
+
+    const endOfLine = document.querySelector(`[data-square-num = "${currLine[4]}"]`)
+    if (endOfLine.innerHTML === "" || endOfLine.innerHTML === circlePreview || endOfLine.innerHTML === crossPreview) {
+        addShapeSlot = document.querySelector(`[data-square-num = "${currLine[4]}"]`)
+    } else {
+        for (let num of currLine) {
+            const selectedSlot = document.querySelector(`[data-square-num = "${num}"]`)
+            if (selectedSlot.innerHTML === cross || selectedSlot.innerHTML === circle) {
+                addShapeSlot = document.querySelector(`[data-square-num = "${num - 5}"]`)
+                break
+            }
+        }
+    }
+
+    if (turn % 2 === 0 && addShapeSlot.innerHTML === "") {
+        addShapeSlot.innerHTML = crossPreview
+    } else if (addShapeSlot.innerHTML === "") {
+        addShapeSlot.innerHTML = circlePreview
+    }
+}
+
+const unPreviewSymbolC4 = (event) => {
+    let currLine = null
+    let addShapeSlot = ""
+    targetNumId = event.target.dataset.squareNum
+
+    for (let line in gameData[currentGridSize].insertLines) {
+        const checkLine = gameData[currentGridSize].insertLines[line]
+        if (checkLine.includes(Number(targetNumId))) {
+            currLine = checkLine
+            break
+        }
+    }
+
+    const endOfLine = document.querySelector(`[data-square-num = "${currLine[4]}"]`)
+    if (endOfLine.innerHTML === "" || endOfLine.innerHTML === circlePreview || endOfLine.innerHTML === crossPreview) {
+        addShapeSlot = document.querySelector(`[data-square-num = "${currLine[4]}"]`)
+    } else {
+        for (let num of currLine) {
+            const selectedSlot = document.querySelector(`[data-square-num = "${num}"]`)
+            if (selectedSlot.innerHTML === cross || selectedSlot.innerHTML === circle) {
+                addShapeSlot = document.querySelector(`[data-square-num = "${num - 5}"]`)
+                break
+            }
+        }
+    }
+    if (addShapeSlot.innerHTML === crossPreview || addShapeSlot.innerHTML === circlePreview) {
+        addShapeSlot.innerHTML = ""
     }
 }
 
@@ -211,22 +371,29 @@ const setLineSize = (value) => {
 }
 
 const whitenSquares = () => {
-    const slots = document.querySelectorAll('.game-slot')
+    const slots = document.querySelectorAll(".game-slot")
     for (let slot of slots) {
-        slot.style.backgroundColor = 'white'
+        slot.style.backgroundColor = "white"
         slot.style.opacity = 0.85
     }
 }
 
 const createBoard = () => {
     for (let i = 0; i < lineSize ** 2; i++) {
-        const newSquare = document.createElement('div')
-        newSquare.classList.add('game-slot')
+        const newSquare = document.createElement("div")
+        newSquare.classList.add("game-slot")
         newSquare.dataset.squareNum = i + 1
-        newSquare.addEventListener('mouseover', previewSymbol)
-        newSquare.addEventListener('mouseleave', unPreviewSymbol)
-        newSquare.addEventListener('click', insertSymbol)
-        gameContainer.appendChild(newSquare)
+        if (currentGridSize !== "Connect 4") {
+            newSquare.addEventListener("mouseover", previewSymbol)
+            newSquare.addEventListener("mouseleave", unPreviewSymbol)
+            newSquare.addEventListener("click", insertSymbol)
+            gameContainer.appendChild(newSquare)
+        } else {
+            newSquare.addEventListener("mouseover", previewSymbolC4)
+            newSquare.addEventListener("mouseleave", unPreviewSymbolC4)
+            newSquare.addEventListener("click", insertSymbolConnect4)
+            gameContainer.appendChild(newSquare)
+        }
     }
 }
 
@@ -234,9 +401,9 @@ const initBoard = () => {
     turn = 0
     crossSlots = []
     circleSlots = []
-    const slots = document.querySelectorAll('.game-slot')
-    const winStuff = document.querySelectorAll('.win')
-    
+    const slots = document.querySelectorAll(".game-slot")
+    const winStuff = document.querySelectorAll(".win")
+
     for (let stuff of winStuff) {
         stuff.remove()
     }
@@ -253,7 +420,7 @@ initBoard()
 whitenSquares()
 
 for (let selector of gridSizeSelectors) {
-    selector.addEventListener('click', setLineSize)
+    selector.addEventListener("click", setLineSize)
 }
 
 for (let selector of gridSizeSelectors) {
@@ -261,45 +428,42 @@ for (let selector of gridSizeSelectors) {
 }
 
 const showHead = () => {
-    if (header.classList.contains('down')) {
+    if (header.classList.contains("down")) {
         for (let selector of gridSizeSelectors) {
             selector.disabled = true
         }
 
-        plug.style.transition = '0.5s'
-        plug.classList.toggle('down')
-        scoreDiv.classList.toggle('down')
-        gridSizeDiv.classList.toggle('down')
-        setTimeout(() => header.classList.toggle('down'), 250)
-    } 
-    else {
+        plug.style.transition = "0.5s"
+        plug.classList.toggle("down")
+        scoreDiv.classList.toggle("down")
+        gridSizeDiv.classList.toggle("down")
+        setTimeout(() => header.classList.toggle("down"), 250)
+    } else {
         for (let selector of gridSizeSelectors) {
             selector.disabled = false
         }
 
-        header.classList.toggle('down')
-        plug.style.transition = '2s'
+        header.classList.toggle("down")
+        plug.style.transition = "2s"
 
         setTimeout(() => {
-            gridSizeDiv.classList.toggle('down')
-            scoreDiv.classList.toggle('down')
-            setTimeout(() => plug.classList.toggle('down'), 300)
+            gridSizeDiv.classList.toggle("down")
+            scoreDiv.classList.toggle("down")
+            setTimeout(() => plug.classList.toggle("down"), 300)
         }, 250)
     }
-
 }
 
 const restartGame = () => {
-    const slots = document.querySelectorAll('.game-slot')
+    const slots = document.querySelectorAll(".game-slot")
     for (let slot of slots) {
-        slot.style.backgroundColor = 'black'
+        slot.style.backgroundColor = "black"
         slot.style.opacity = 0
     }
-    gameContainer.style.zIndex = 1;
-    winTextDiv.style.opacity = 0;
+    winTextDiv.style.opacity = 0
     setTimeout(() => initBoard(), 400)
     setTimeout(() => whitenSquares(), 800)
 }
 
-dropDown.addEventListener('click', showHead)
-restartButton.addEventListener('click', restartGame)
+dropDown.addEventListener("click", showHead)
+restartButton.addEventListener("click", restartGame)
